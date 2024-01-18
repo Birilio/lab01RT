7eef128622117d31f663d8c88894835962a1b5ff
Birilio <71884774+Birilio@users.noreply.github.com>
2024-01-18 11:26:18 +0100

    update i2c + separate Queen & RTtests

--- scriptInit.sh
+++ scriptInit.sh
@@ -1,8 +1,9 @@
 uname -ra
 sudo apt-get update --allow-releaseinfo-change
 sudo apt-get upgrade
-sudo apt install python3-pip git build-essential libnuma-dev
-pip3 install tqdm
+sudo apt install python3-pip git build-essential libnuma-dev i2c-tools python-smbus
+sudo pip3 install tqdm adafruit-pca9685
+sudo raspi-config nonint do_i2c 0
 mkdir lab01
 cd lab01
 git clone https://github.com/Birilio/lab01RT.git
@@ -11,3 +12,4 @@ cd rt-tests
 git checkout stable/v1.0
 make all
 make install
+sudo i2cdetect -y 1
---
+++ scriptNQueens.sh
@@ -0,0 +1,3 @@
+cd N-Queens-Problem-master
+python3 queenpool_multithread.py multithread_output.csv 12 4 100
+python3 queenpool_multithread.py singlethread_output.csv 12 1 100
